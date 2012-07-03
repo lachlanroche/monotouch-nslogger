2a5fbfea13beaba641d76016136f370490b6d282
Lachlan Roche <hello@lachlan.roche.name>
2012-07-03 13:23:04 +1000

    btouch requires another file before AssemblyInfo.cs, or it will drop the LinkWith attribute

--- binding/AssemblyInfo.cs
+++ binding/AssemblyInfo.cs
@@ -1,3 +1,4 @@
+using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly:LinkWith("libnslogger.a", LinkTarget.ArmV6 | LinkTarget.ArmV7 | LinkTarget.Simulator, Frameworks= "CFNetwork SystemConfiguration", ForceLoad = true, IsCxx = false, NeedsGccExceptionHandling=true)]
+[assembly:LinkWith("libnslogger.a", LinkTarget.ArmV6 | LinkTarget.ArmV7 | LinkTarget.Simulator, Frameworks = "CFNetwork SystemConfiguration", ForceLoad = true, IsCxx = false, NeedsGccExceptionHandling = false)]
